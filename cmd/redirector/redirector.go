8b55c7223d8f7053932010764a0796e393f9755f
Tony Garcia <tonysk8@gmx.net>
2020-06-15 23:18:55 -0500

    Add redirector

---
+++ cmd/redirector/redirector.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+// redirect returns a 301 permanent redirect on https
+func redirect(w http.ResponseWriter, r *http.Request) {
+	l := "https://" + r.Host + r.RequestURI
+
+	log.Println(r.RemoteAddr, l)
+
+	w.Header().Add("Location", l)
+	w.WriteHeader(http.StatusMovedPermanently)
+}
+
+func main() {
+	listenPort := ":8080"
+	lp := os.Getenv("LISTEN_PORT")
+	if lp != "" {
+		if _, err := strconv.Atoi(lp); err == nil {
+			listenPort = ":" + lp
+		}
+	}
+	http.HandleFunc("/", redirect)
+	log.Fatal(http.ListenAndServe(listenPort, nil))
+}
